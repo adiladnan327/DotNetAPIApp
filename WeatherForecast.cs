7409bddc6ca6eab9c69635fe9cb7c022a877bb14
M Adil <48308553+adiladnan327@users.noreply.github.com>
2025-01-30 13:31:27 -0500

    Update WeatherForecast.cs
    Added the Feels like class

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace DotNetAPIApp
 
         public int TemperatureC { get; set; }
 
+         public int FeelsLike { get; set; }
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
